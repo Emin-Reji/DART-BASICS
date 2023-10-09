7a9ccdbd793f1998df499d0c5c97922dc0a29de0
Emin.Reji <eminreji22@gmail.com>
2023-10-09 18:34:59 +0530

    commit

---
+++ Functions.dart
@@ -0,0 +1,26 @@
+void main(){
+        // //Functions
+        // myfunction(){
+        //     return("Hello");
+        // }                                Output:Hello 
+        // var thing = myfunction();
+        // print(thing);
+// ---------------------------------------------------------------
+//         myfunction(String name){
+//             return "Hello $name";
+//         }                                Output:Hello Emin
+//         var thing = myfunction("Emin");
+//         print(thing);
+//-----------------------------------------------------------------
+//      myfunction(String name1, name2){
+//      return "Hello $name1 and $name2"; 
+//     }                                   Output:Hello Emin and Eminr
+//      var thing=myfunction("Emin","Eminr");
+//      print(thing);
+//------------------------------------------------------------------
+//        myfunction(String name1, {name2="friends"}){
+//            return"Hello $name1 and $name2";
+//       }                                 Output:Hello Emin and friends/Hello Emin and Bill
+//       var thing = myfunction("Emin", name2:"Bill");
+//        print(thing);
+}
