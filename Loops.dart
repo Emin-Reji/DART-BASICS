be9a31c676ec2fe806ac04040283a14316d1e398
Emin.Reji <eminreji22@gmail.com>
2023-10-09 17:14:32 +0530

    commit

---
+++ Loops.dart
@@ -0,0 +1,22 @@
+void main(){
+    //loops
+    //For loops
+    var num = 5;
+    for(var i = num; i >= 1; i--){
+        print(i);
+    }
+
+    //For In loop
+    var names =["Emin","John","Teena"];
+    for(var name in names){
+        print(name);
+    }
+
+    //while loop
+    while(num >= 1){
+        print(num);
+        num--;
+        
+    }
+
+}
