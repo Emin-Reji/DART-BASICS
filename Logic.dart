d32dfa88d25137676017b6b1a457ad948293149c
Emin.Reji <eminreji22@gmail.com>
2023-10-09 17:27:58 +0530

    commit

---
+++ Logic.dart
@@ -0,0 +1,28 @@
+void main(){
+    //Logic- if, if else,if else if
+    //var num = 5;
+    // if(num == 5){
+    //     print("The number is 5");
+    // }
+
+    //if else
+    // var number=4;
+    // if(number == 5){
+    //     print("The number is 5");
+    // } else{
+    //     print("The number is not 5!");
+    // }
+
+    // if else if
+    var num=3;
+    if(num ==5){
+        print("The number is 5");
+    }
+    else if (num == 3){
+        print("The number is 3");
+    }
+    else{
+        print("The number is NOT 5!");
+    }
+
+}
