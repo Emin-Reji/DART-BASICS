84379fead3f5c2f15901f464c30da38a3b488be6
Emin.Reji <eminreji22@gmail.com>
2023-10-11 11:46:41 +0530

    commit

---
+++ Switch.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+void main(){
+    print("Enter a input");
+    var input=int.parse(stdin.readLineSync()!);
+    switch(input){
+        case 1: print("Sunday");
+        break;
+
+        case 2: print("Monday");
+        break;
+
+        case 3: print("Tuesday");
+        break;
+
+        case 4: print("Wednesday");
+        break;
+        
+        case 5: print("Thursday");
+        break;
+
+        case 6: print("Friday");
+        break;
+
+        case 7: print("Saturday");
+        break;
+
+        default: print("Invalid entry");
+        break;
+    }
+}
